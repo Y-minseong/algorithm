26bacecc52a2c6f82c7e471ea5da179fa8f3164a
윤민성 <dbsalstj2310@gmail.com>
2024-07-17 10:03:18 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 400 ms, Memory: 137532 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,45 +1,34 @@
 import sys
-input = sys.stdin.read
+sys.setrecursionlimit(10**4)
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+for i in range(1, v + 1):
+    parent[i] = i
 
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
-def union_parent(parent, rank, a, b):
-    root_a = find_parent(parent, a)
-    root_b = find_parent(parent, b)
-    if root_a != root_b:
-        if rank[root_a] > rank[root_b]:
-            parent[root_b] = root_a
-        else:
-            parent[root_a] = root_b
-            if rank[root_a] == rank[root_b]:
-                rank[root_b] += 1
-
-data = input().split()
-v = int(data[0])
-e = int(data[1])
-parent = [i for i in range(v + 1)]
-rank = [0] * (v + 1)
-
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 edges = []
 total_cost = 0
-index = 2
 
 for _ in range(e):
-    a = int(data[index])
-    b = int(data[index + 1])
-    cost = int(data[index + 2])
+    a, b, cost = map(int, input().split())
     edges.append((cost, a, b))
-    index += 3
-
-# 간선을 가중치 기준으로 정렬
 edges.sort()
-
-for cost, a, b in edges:
+    
+for edge in edges:
+    cost, a , b = edge
+    
     if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, rank, a, b)
+        union_parent(parent, a, b)
         total_cost += cost
-
 print(total_cost)
