59d03cee78de00b0e66d198c7462df85eb9c47a1
윤민성 <dbsalstj2310@gmail.com>
2024-07-19 13:56:01 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 113944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,30 +1,30 @@
 from collections import deque
-
-N, M = map(int, input().split())
+N , M = map(int, input().split())
 board = [input().strip() for _ in range(N)]
-dx = (1, 0, -1, 0)
-dy = (0, 1, 0, -1)
+dx = (1, 0, -1, 0) # x 행렬
+dy = (0, 1, 0, -1) # y 행렬
 
-def is_valid_coord(y, x):
-    return 0 <= y < N and 0 <= x < M
 
-def bfs():
-    chk = [[False] * M for _ in range(N)]
-    chk[0][0] = True
-    dq = deque()
-    dq.append((0, 0, 1))
-    while dq:
-        y, x, d = dq.popleft()
+def bfs(): # 함수 실행
+    visited = [[False] * M for _ in range(N)] # 방문 기록 초기화
+    visited[0][0] = True # [0][0]을 방문 입력
+    dq = deque() # 큐 생성
+    dq.append((0, 0, 1)) # y값과 x값 그리고 카운트 append()
+    while dq: # 큐가 빌 때까지 실행
+        y, x, d = dq.popleft() # 큐에서 추출
         
-        if y == N - 1 and x == M - 1:
+        if y == N - 1 and x == M - 1: # 기저 조건
             return d
         
-        for k in range(4):
-            ny = y + dy[k]
-            nx = x + dx[k]
-            if is_valid_coord(ny, nx) and board[ny][nx] == '1' and not chk[ny][nx]:
-                chk[ny][nx] = True
-                dq.append((ny, nx, d + 1))
-    return -1  # 경로가 없을 경우
-
+        for i in range(4):
+            ny = y + dy[i] # 현재 위치한 행렬 옮기기
+            nx = x + dx[i]
+            # 행렬을 넘지 않는지, 1로 되어있는지 그리고 방문기록이 없는지 확인
+            if 0 <= ny < N and 0 <= nx < M and board[ny][nx] == '1' and not visited[ny][nx]:
+                visited[ny][nx] = True # 조건문이 통과됬다면 True로 교체
+                dq.append((ny, nx, d + 1)) # 큐에 추가
+    return - 1
+                
 print(bfs())
+     
+
