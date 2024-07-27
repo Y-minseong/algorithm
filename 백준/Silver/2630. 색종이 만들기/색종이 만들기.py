565dbcb6eec42f88239aceb24592747af11cd53d
윤민성 <dbsalstj2310@gmail.com>
2024-07-27 15:10:21 +0900

    [Silver II] Title: 색종이 만들기, Time: 148 ms, Memory: 111948 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,41 +1,41 @@
-# 색종이 만들기
 import sys
 
-def is_same_colour(x, y, size):
-    same_color = True
-    initial_color = paper[x][y]
-    # 현재 영역의 모든 칸을 검사하여 같은 색인지 확인
-    for i in range(x, x + size):
-        for j in range(y, y + size):
-            if paper[i][j] != initial_color:  # 다른 색이 발견되면
-                same_color = False  # 같은 색이 아님을 표시
-                break  # 내부 루프 중지
-        if not same_color:  # 같은 색이 아님이 확인되면
-            break  # 외부 루프 중지
-    return same_color
+# input 값을 받는다.
+N = int(sys.stdin.readline())
+paper = []
+for i in range(N):
+    a = list(map(int, sys.stdin.readline().split()))
+    paper.append(a)
+blue, white = 0, 0
 
-def count_colors(x, y, size):
-    global white, blue
-    initial_color = paper[x][y]  # 현재 영역의 첫 번째 색상
-    same_color = is_same_colour(x, y, size)  # 현재 영역이 모두 같은 색인지 확인하는 플래그
+def quarter_square(x, y, n):
+    global paper, blue, white
+    check = True
+    first_color = paper[x][y]
+    
+    for i in range(x, x+n):
+       
+        for j in range(y, y+n):
+            if paper[i][j] != first_color:
+                check = False
+                break
+        if not check: # 기저조건이 이거인 이유.
+            break
+                
+        
+    if check:
+        if first_color == 1:
+            blue += 1
+            
+        else:
+            white += 1
 
-    if same_color:  # 영역이 모두 같은 색인 경우
-        if initial_color == 0:  # 흰색일 경우
-            white += 1  # 흰색 카운트 증가
-        else:  # 파란색일 경우
-            blue += 1  # 파란색 카운트 증가
-    else:  # 영역이 다른 색이 섞여 있는 경우
-        half_size = size // 2  # 영역을 네 부분으로 나눌 크기 계산
-        count_colors(x, y, half_size)  # 1사분면
-        count_colors(x, y + half_size, half_size)  # 2사분면
-        count_colors(x + half_size, y, half_size)  # 3사분면
-        count_colors(x + half_size, y + half_size, half_size)  # 4사분면
-
-if __name__ == '__main__':
-    n = int(sys.stdin.readline())  # 색종이의 크기 입력 받기
-    paper = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]  # 색종이의 색상 입력 받기
-
-    white, blue = 0, 0  # 흰색과 파란색 카운트 초기화
-    count_colors(0, 0, n)  # 전체 색종이에 대해 함수 호출
-    print(white)  # 흰색 색종이 조각 개수 출력
-    print(blue)  # 파란색 색종이 조각 개수 출력
+    else:
+        k = n//2
+        quarter_square(x, y, k)
+        quarter_square(x, y + k, k)
+        quarter_square(x + k, y, k)
+        quarter_square(x + k, y + k, k)
+quarter_square(0, 0, N)
+print(white)
+print(blue)
